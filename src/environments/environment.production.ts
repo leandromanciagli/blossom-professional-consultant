b3fb35df5f04677ff9491d85d127c0ba5406b951
lmanciagli <leandro.manciagli@gba.gob.ar>
2025-05-03 15:52:38 -0300

    Change public key

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     apiUrl: 'https://blossomconsultoraprofesional.net/wp-json',
-    publicKey: 'APP_USR-3bf4f6da-90ba-4db9-a4e9-48df95b4fb53',
+    publicKey: 'APP_USR-7052efb5-a773-4372-8486-50b8a78f6fe6',
 };
